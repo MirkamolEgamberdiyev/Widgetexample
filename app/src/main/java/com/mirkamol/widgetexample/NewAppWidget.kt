5b0bbbdc563e3dd039f47810e6026fee6f7e2091
Mirkamol <mirkamol9187907@gmail.com>
2022-07-14 11:33:23 +0500

    first commit

---
+++ app/src/main/java/com/mirkamol/widgetexample/NewAppWidget.kt
@@ -0,0 +1,57 @@
+package com.mirkamol.widgetexample
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
+import android.util.Log
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class NewAppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+        Log.d("TAG", "onEnabled: ")
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.getString(R.string.appwidget_text)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    val intent = Intent(ACTION_VIEW, Uri.parse("http://ok.ru"))
+
+    val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
